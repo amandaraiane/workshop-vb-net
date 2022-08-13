2ca05f70f99ee86dd85e7051557300b67ad2f6eb
amanda raiane <amanda127alves@gmail.com>
2022-08-13 17:25:38 -0300

    Cadastro de alunos

--- CadastroAlunoVB/frmCadastroAluno.Designer.vb
+++ CadastroAlunoVB/frmCadastroAluno.Designer.vb
@@ -27,7 +27,35 @@ Partial Class frmCadastroAluno
         Me.ContextMenuStrip2 = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SairToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.txtNome = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtTelefone = New System.Windows.Forms.TextBox()
+        Me.btnIncluir = New System.Windows.Forms.Button()
+        Me.btnAtualizar = New System.Windows.Forms.Button()
+        Me.btnLimpar = New System.Windows.Forms.Button()
+        Me.btnExcluir = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnPesquisar = New System.Windows.Forms.Button()
+        Me.txtCodigoPesquisa = New System.Windows.Forms.TextBox()
+        Me.txtNomePesquisa = New System.Windows.Forms.TextBox()
+        Me.colCodigo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colNome = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colTelefone = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.ContextMenuStrip2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ContextMenuStrip1
@@ -51,17 +79,249 @@ Partial Class frmCadastroAluno
         'SairToolStripMenuItem
         '
         Me.SairToolStripMenuItem.Name = "SairToolStripMenuItem"
-        Me.SairToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SairToolStripMenuItem.Size = New System.Drawing.Size(92, 22)
         Me.SairToolStripMenuItem.Text = "sair"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.GroupBox1.Controls.Add(Me.txtTelefone)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtNome)
+        Me.GroupBox1.Controls.Add(Me.txtCodigo)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(748, 155)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Dados"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.Window
+        Me.GroupBox2.Controls.Add(Me.btnLimpar)
+        Me.GroupBox2.Controls.Add(Me.btnExcluir)
+        Me.GroupBox2.Controls.Add(Me.btnAtualizar)
+        Me.GroupBox2.Controls.Add(Me.btnIncluir)
+        Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox2.Location = New System.Drawing.Point(0, 155)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(748, 101)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Comandos"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.GroupBox3.Controls.Add(Me.DataGridView1)
+        Me.GroupBox3.Controls.Add(Me.Panel1)
+        Me.GroupBox3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox3.Location = New System.Drawing.Point(0, 256)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(748, 280)
+        Me.GroupBox3.TabIndex = 4
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Pesquisa"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Código do Aluno"
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(16, 36)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(83, 20)
+        Me.txtCodigo.TabIndex = 1
+        '
+        'txtNome
+        '
+        Me.txtNome.Location = New System.Drawing.Point(125, 36)
+        Me.txtNome.Name = "txtNome"
+        Me.txtNome.Size = New System.Drawing.Size(406, 20)
+        Me.txtNome.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(122, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nome"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Telefone"
+        '
+        'txtTelefone
+        '
+        Me.txtTelefone.Location = New System.Drawing.Point(16, 101)
+        Me.txtTelefone.Name = "txtTelefone"
+        Me.txtTelefone.Size = New System.Drawing.Size(249, 20)
+        Me.txtTelefone.TabIndex = 5
+        '
+        'btnIncluir
+        '
+        Me.btnIncluir.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.btnIncluir.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnIncluir.Location = New System.Drawing.Point(15, 42)
+        Me.btnIncluir.Name = "btnIncluir"
+        Me.btnIncluir.Size = New System.Drawing.Size(75, 23)
+        Me.btnIncluir.TabIndex = 0
+        Me.btnIncluir.Text = "Incluir"
+        Me.btnIncluir.UseVisualStyleBackColor = False
+        '
+        'btnAtualizar
+        '
+        Me.btnAtualizar.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnAtualizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnAtualizar.Location = New System.Drawing.Point(112, 42)
+        Me.btnAtualizar.Name = "btnAtualizar"
+        Me.btnAtualizar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAtualizar.TabIndex = 0
+        Me.btnAtualizar.Text = "Atualizar"
+        Me.btnAtualizar.UseVisualStyleBackColor = False
+        '
+        'btnLimpar
+        '
+        Me.btnLimpar.BackColor = System.Drawing.Color.Khaki
+        Me.btnLimpar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnLimpar.Location = New System.Drawing.Point(312, 42)
+        Me.btnLimpar.Name = "btnLimpar"
+        Me.btnLimpar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpar.TabIndex = 0
+        Me.btnLimpar.Text = "Limpar"
+        Me.btnLimpar.UseVisualStyleBackColor = False
+        '
+        'btnExcluir
+        '
+        Me.btnExcluir.BackColor = System.Drawing.Color.LightCoral
+        Me.btnExcluir.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExcluir.Location = New System.Drawing.Point(210, 42)
+        Me.btnExcluir.Name = "btnExcluir"
+        Me.btnExcluir.Size = New System.Drawing.Size(75, 23)
+        Me.btnExcluir.TabIndex = 0
+        Me.btnExcluir.Text = "Excluir"
+        Me.btnExcluir.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.txtNomePesquisa)
+        Me.Panel1.Controls.Add(Me.txtCodigoPesquisa)
+        Me.Panel1.Controls.Add(Me.btnPesquisar)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(3, 16)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(742, 79)
+        Me.Panel1.TabIndex = 0
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colCodigo, Me.colNome, Me.colTelefone})
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 95)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(742, 182)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 14)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Código"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(119, 14)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 13)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Nome"
+        '
+        'btnPesquisar
+        '
+        Me.btnPesquisar.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.btnPesquisar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnPesquisar.Location = New System.Drawing.Point(427, 31)
+        Me.btnPesquisar.Name = "btnPesquisar"
+        Me.btnPesquisar.Size = New System.Drawing.Size(75, 23)
+        Me.btnPesquisar.TabIndex = 3
+        Me.btnPesquisar.Text = "Pesquisar"
+        Me.btnPesquisar.UseVisualStyleBackColor = False
+        '
+        'txtCodigoPesquisa
+        '
+        Me.txtCodigoPesquisa.Location = New System.Drawing.Point(13, 31)
+        Me.txtCodigoPesquisa.Name = "txtCodigoPesquisa"
+        Me.txtCodigoPesquisa.Size = New System.Drawing.Size(64, 20)
+        Me.txtCodigoPesquisa.TabIndex = 4
+        '
+        'txtNomePesquisa
+        '
+        Me.txtNomePesquisa.Location = New System.Drawing.Point(122, 31)
+        Me.txtNomePesquisa.Name = "txtNomePesquisa"
+        Me.txtNomePesquisa.Size = New System.Drawing.Size(266, 20)
+        Me.txtNomePesquisa.TabIndex = 5
+        '
+        'colCodigo
+        '
+        Me.colCodigo.HeaderText = "Código"
+        Me.colCodigo.Name = "colCodigo"
+        Me.colCodigo.ReadOnly = True
+        '
+        'colNome
+        '
+        Me.colNome.HeaderText = "Nome"
+        Me.colNome.Name = "colNome"
+        Me.colNome.ReadOnly = True
+        '
+        'colTelefone
+        '
+        Me.colTelefone.HeaderText = "Telefone"
+        Me.colTelefone.Name = "colTelefone"
+        Me.colTelefone.ReadOnly = True
         '
         'frmCadastroAluno
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(748, 536)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "frmCadastroAluno"
         Me.Text = "frmCdastroAluno"
         Me.ContextMenuStrip2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -70,4 +330,27 @@ Partial Class frmCadastroAluno
     Friend WithEvents ContextMenuStrip2 As ContextMenuStrip
     Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents SairToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtNome As TextBox
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents txtTelefone As TextBox
+    Friend WithEvents btnAtualizar As Button
+    Friend WithEvents btnIncluir As Button
+    Friend WithEvents btnLimpar As Button
+    Friend WithEvents btnExcluir As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtNomePesquisa As TextBox
+    Friend WithEvents txtCodigoPesquisa As TextBox
+    Friend WithEvents btnPesquisar As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents colCodigo As DataGridViewTextBoxColumn
+    Friend WithEvents colNome As DataGridViewTextBoxColumn
+    Friend WithEvents colTelefone As DataGridViewTextBoxColumn
 End Class
--- CadastroAlunoVB/frmCadastroAluno.vb
+++ CadastroAlunoVB/frmCadastroAluno.vb
@@ -1,5 +1,63 @@
 ﻿Public Class frmCadastroAluno
+    Dim strConexao As String = "Data Source=(local)\sqlexpress;UID=nutec;PWD=nutec; Initial Catalog=Nutec;"
     Private Sub frmCadastroAluno_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub GroupBox3_Enter(sender As Object, e As EventArgs) Handles GroupBox3.Enter
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub btnAtualizar_Click(sender As Object, e As EventArgs) Handles btnAtualizar.Click, btnLimpar.Click, btnExcluir.Click
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub btnIncluir_Click(sender As Object, e As EventArgs) Handles btnIncluir.Click
+        Dim conexao As New System.Data.SqlClient.SqlConnection
+        conexao.ConnectionString = strConexao
+        conexao.Open()
+
+        Dim comando As New System.Data.SqlClient.SqlCommand
+        comando.Connection = conexao
+        comando.CommandText = String.Format("Insert Into Alunos (Nome, Telefone) Values ('{0}', '{1}')", txtNome.Text, txtTelefone.Text)
+        comando.ExecuteNonQuery()
+
+        conexao.Close()
+
+    End Sub
+
+    Private Sub btnPesquisar_Click(sender As Object, e As EventArgs) Handles btnPesquisar.Click
+        Dim conexao As New System.Data.SqlClient.SqlConnection
+        conexao.ConnectionString = strConexao
+        conexao.Open()
+
+        Dim comando As New System.Data.SqlClient.SqlCommand
+        comando.Connection = conexao
+        comando.CommandText = String.Format("Select * from Alunos")
+
+        Dim banco As New DataSet
+        Dim adaptadordeDados As New System.Data.SqlClient.SqlDataAdapter(comando.CommandText, conexao)
+        adaptadordeDados.Fill(banco)
+
+        conexao.Close()
+
+        DataGridView1.DataSource = banco.Tables(0)
+        DataGridView1.Refresh()
     End Sub
 End Class
